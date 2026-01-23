207db8ea509f84eb2fa5d1e78b03023896df669d
Wadamir <domus159@gmail.com>
2026-01-24 01:37:40 +0500

    fix wrong youtube thumbnail

--- bot/main.py
+++ bot/main.py
@@ -100,6 +100,9 @@ BASE_URL = os.getenv("BASE_URL", "http://localhost:8080")
 
 COOKIES_PATH = "/cookies.txt"
 
+ASSETS_DIR = Path(__file__).parent / "assets"
+PLACEHOLDER_THUMBNAIL = ASSETS_DIR / "youtube_placeholder.jpg"
+
 if APP_ENV == "dev":
 	logging.info("Running in DEV mode")
 else:
@@ -615,13 +618,23 @@ async def process_job(job: dict):
 		# --- Telegram upload possible ---
 		if real_size_mb <= MAX_TG_AUDIO_EFFECTIVE_MB:
 			try:
+				# 1. Try YouTube thumbnail
 				thumb_msg = None
 				thumb_url = info.get("thumbnail")
 				if thumb_url:
-					# --- Send thumbnail ---
-					thumb_msg = await status_msg.reply_photo(
-						photo=thumb_url,
-					)
+					try:
+						# --- Send thumbnail ---
+						thumb_msg = await status_msg.reply_photo(photo=thumb_url)
+					except Exception as e:
+						logging.warning(f"Failed to send thumbnail: {e}")
+
+				# 2. Fallback to local placeholder
+				if not thumb_msg and PLACEHOLDER_THUMBNAIL.exists():
+					try:
+						with open(PLACEHOLDER_THUMBNAIL, "rb") as f:
+							thumb_msg = await status_msg.reply_photo(photo=f)
+					except Exception as e:
+						logging.warning(f"Placeholder thumbnail failed: {e}")
 
 				# --- Send audio ---
 				with open(tmp_path, "rb") as f:
@@ -631,11 +644,17 @@ async def process_job(job: dict):
 						performer=info.get("uploader"),
 						duration=duration,
 						filename=build_audio_filename(info),
+						thumb = PLACEHOLDER_THUMBNAIL if PLACEHOLDER_THUMBNAIL.exists() else None,
 						caption=f"{BOT_CAPTION} <b>{BOT_USERNAME}</b>",
 						parse_mode="HTML",
 						reply_to_message_id=thumb_msg.message_id if thumb_msg else None,
 					)
-				await status_msg.delete()
+
+				try:
+					await status_msg.delete()
+				except Exception:
+					logging.warning("Failed to delete status message after upload")
+					pass
 
 				increment_downloads(user.id)
 
@@ -670,7 +689,13 @@ async def process_job(job: dict):
 
 			except Exception as e:
 				logging.exception("Telegram upload failed")
-				await status_msg.edit_text("❌ Failed to send audio.")
+
+				try:
+					await status_msg.edit_text("❌ Failed to send audio.")
+				except Exception:
+					logging.warning("Failed to edit status message after upload failure")
+					pass
+
 				try:
 					log_download(
 						user_id=user.id,
