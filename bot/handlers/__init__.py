78fb5930fc7a78f0545727fff3755c1d29713b39
Wadamir <domus159@gmail.com>
2026-01-24 20:56:53 +0500

    fully changed architecture

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,35 +1,23 @@
+from telegram.ext import Application
 from telegram.ext import (
-	Application,
 	CommandHandler,
 	CallbackQueryHandler,
 	MessageHandler,
 	filters,
 )
 
-from .commands import (
-	start_handler,
-	plan_handler,
-	stats_handler,
-)
-
-from .callbacks import (
-	language_callback,
-)
-
-from .messages import (
-	handle_message,
-)
+from .commands import start_handler, plan_handler
+from .callbacks import language_callback
+from .messages import handle_message
+from bot.admin.stats import stats_handler
 
 __all__ = ["register_handlers"]
 
 def register_handlers(app: Application):
-	# Command handlers
 	app.add_handler(CommandHandler("start", start_handler))
 	app.add_handler(CommandHandler("plan", plan_handler))
 	app.add_handler(CommandHandler("stats", stats_handler))
 
-	# Callback handlers
 	app.add_handler(CallbackQueryHandler(language_callback, pattern="^lang_"))
-	
-	# Message handlers
 	app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+
