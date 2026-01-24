9843a7943620c6e8e0973170f4ee37d34f7cd0c3
Wadamir <domus159@gmail.com>
2026-01-24 21:37:40 +0500

    move main_old to lagcy

--- bot/workers/downloader.py
+++ bot/workers/downloader.py
@@ -10,6 +10,6 @@ async def process_job(job: Dict):
 	Process single download job.
 	All heavy logic lives here.
 	"""
-	from bot.main_old import _process_job_impl  # временно, см. ниже
+	from bot._legacy.main_old import _process_job_impl  # временно, см. ниже
 
 	return await _process_job_impl(job)
