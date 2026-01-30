91e3735e798ece5ad10f7d912471bf7e2c8d86ff
Wadamir <domus159@gmail.com>
2026-01-30 17:39:33 +0500

    fix proxy IP

--- bot/config/network.py
+++ bot/config/network.py
@@ -1,4 +1,4 @@
 import os
 
-YT_PROXY_PRIMARY = os.getenv("YT_PROXY_PRIMARY", "socks5://host.docker.internal:1080")
-YT_PROXY_SECONDARY = os.getenv("YT_PROXY_SECONDARY", "socks5://host.docker.internal:1081")
+YT_PROXY_PRIMARY = os.getenv("YT_PROXY_PRIMARY", "socks5h://ytaudio_net:1080")
+YT_PROXY_SECONDARY = os.getenv("YT_PROXY_SECONDARY", "socks5h://ytaudio_net:1081")
