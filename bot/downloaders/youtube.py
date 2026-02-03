96cdce985a9168fc7a47cdcb4107164ab204e5f0
Wadamir <domus159@gmail.com>
2026-02-03 18:45:28 +0500

    fix proxy - for test

--- bot/downloaders/youtube.py
+++ bot/downloaders/youtube.py
@@ -129,6 +129,7 @@ class YouTubeDownloader(BaseDownloader):
             },
 		}
 		if os.getenv("APP_ENV") == "prod":
+			return
 			if self._should_use_secondary_ip():
 				logger.info("[downloader] Using secondary IP for yt-dlp")
 				opts["proxy"] = YT_PROXY_SECONDARY
